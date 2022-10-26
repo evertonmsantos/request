f26b859138d2d475ad12798b922e6143460a15f8
Everton Santos <59515378+evertonmsantos@users.noreply.github.com>
2022-10-26 09:42:55 -0300

    Update delete.go
    DisableCompression Removido

--- delete.go
+++ delete.go
@@ -24,7 +24,6 @@ func Delete(url, postdata string, headers map[string]string, followlocation bool
 		Timeout: 60 * time.Second,
 		Transport: &http.Transport{
 			IdleConnTimeout:    60 * time.Second,
-			DisableCompression: true,
 		},
 	}
 
